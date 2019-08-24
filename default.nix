af68f25184eac73d1c84eea53297630154539cee
Oren Rozen <countoren@gmail.com>
2019-08-24 11:41:05 -0500

    change haskell-src-exts nix dependency to be fetched from github(version 1.20.3)

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
 let
   pkgs = import ./nixpkgs.nix {};
 in
-  pkgs.haskellPackages.callPackage ./project.nix { }
+  pkgs.haskellPackages.callPackage ./project.nix {
+    haskell-src-exts = import (pkgs.fetchFromGitHub {
+                     owner="countoren";
+                     repo="haskell-src-exts";
+                     rev="c3464f21bae77a6b5e620ca172d056dbb0d298dd";
+                     sha256="09mdssksa15n8rkvis1s69mim12lhl7c4s2hg7cs5j4z3bbhr5vw";
+    });
+  }
+
+  
